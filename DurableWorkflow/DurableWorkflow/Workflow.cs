ed31ba0d04784456e8fa9fc07fe903bd60feda91
Aaron Powell <me@aaron-powell.com>
2019-03-28 16:01:56 +1100

    creating a .NET version

---
+++ DurableWorkflow/DurableWorkflow/Workflow.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace DurableWorkflow
+{
+    public static class Workflow
+    {
+        public static string EventName = "foo";
+
+        [FunctionName("Workflow")]
+        public static async Task Run(
+            [OrchestrationTrigger]DurableOrchestrationContext context,
+            ILogger logger
+            )
+        {
+            var input = context.GetInput<string>();
+            logger.LogInformation($"Starting workflow for {input}");
+
+            await context.WaitForExternalEvent(EventName);
+
+            logger.LogInformation($"Workflow for {input} is stopping");
+        }
+    }
+}
